fedd65fa73c76b643831513c8e6b55a86b61b93a
蒋轩哲 <jgcm@live.cn>
2017-04-23 20:36:23 +0800

    Dynamic Cell Sizing

--- TwitterJGCM/HomeDatasourceController.swift
+++ TwitterJGCM/HomeDatasourceController.swift
@@ -19,8 +19,22 @@ class HomeDataSourceController: DatasourceController {
         self.datasource = homeDataSource
     }
     
+    // cell间距
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 150)
+        
+        if let user = self.datasource?.item(indexPath) as? User {
+            
+            let size = CGSize(width: view.frame.width - 12 - 50 - 12 - 2, height: 1000)
+            
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName:UIFont.systemFont(ofSize: 15)], context: nil)
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        }
+        
+        return CGSize(width: view.frame.width, height: 200)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
