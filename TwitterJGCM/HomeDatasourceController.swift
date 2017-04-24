8b858cd32a2584a4cdf91018054f8416ff4961b4
蒋轩哲 <jgcm@live.cn>
2017-04-24 23:55:55 +0800

    Custom Navigation Bar

--- TwitterJGCM/HomeDatasourceController.swift
+++ TwitterJGCM/HomeDatasourceController.swift
@@ -15,8 +15,35 @@ class HomeDataSourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationItems()
+        
         let homeDataSource = HomeDataSource()
         self.datasource = homeDataSource
+    }
+    
+    func setupNavigationItems() {
+        let titleView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
+        titleView.contentMode = .scaleAspectFit
+        titleView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        self.navigationItem.titleView = titleView
+        
+        let followButton = UIButton(type: .system)
+        followButton.setImage(#imageLiteral(resourceName: "follow"), for: .normal)
+        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+        
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "search"), for: .normal)
+        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "write"), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem.init(customView: composeButton), UIBarButtonItem.init(customView: searchButton)]
+        
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
     }
     
     // cell间距
